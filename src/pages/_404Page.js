0fdce89b4c6202af2de13dad4e725e273bc8eef8
lizheng <v-lizheng@expedia.com>
2020-09-14 11:16:48 +0800

    init pages

---
+++ src/pages/_404Page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class _404Page extends Component {
+  render() {
+    return (
+      <div>
+        _404Page
+      </div>
+    )
+  }
+}
